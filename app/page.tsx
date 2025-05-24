3915f049c633d3f80fea13f4145c6af307810494
swastideep maharana <swastideep67maharana@gmail.com>
2025-05-24 19:16:35 +0530

    #routes done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <>
+      <h1 className="text-2xl underline">Welcom to my SaaS App</h1>
+      <Button>Let's get started</Button>
+    </>
+  );
+};
 
-export default Page
+export default Page;
